de521dc6a8a0c598444b2ea4e504b9e46c6984f5
KrishR-Cedcoss <krishnamrastogi@cedcoss.com>
2022-12-14 14:59:40 +0530

    custom usefetch hook added

---
+++ src/components/UseFetch/UseFetch.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+type dataFromUser = {
+    url: string
+    payload?: string | any
+    method?: string
+}
+
+export const useFetch = () => {
+    let [data, setData] = useState('');
+
+    const extractDataFromApi = (data: dataFromUser) => {
+
+        fetch(data.url, {
+            method: data.method,
+            body: { ...data.payload }
+        })
+            .then(response => response.json())
+            .then(res => {
+                setData(res)
+            })
+            .catch(err => console.log(err))
+    }
+
+    return { data, extractDataFromApi }
+}
