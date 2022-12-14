321d724f38c5de1bb508d3a8ed1c9adf518de3b1
Harsh0299-cedcoss <harshsharma2022jun@cedcoss.com>
2022-12-14 15:48:53 +0530

    Added Contextual Save Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
 import React from "react";
 import Button from "./components/Button/Button";
+import ContextualSaveBar from "./components/ContextualSaveBar/ContextualSaveBar";
 import Pagination from "./components/Pagination/Pagination";
 
-import TextField from './components/Textfield/Textfield';
-import Wrapper from './components/Wrapper/Wrapper';
+import TextField from "./components/Textfield/Textfield";
+import Wrapper from "./components/Wrapper/Wrapper";
 
 function App() {
   return (
     <>
-<Button variant="contained">Button By Harsh</Button>
-      <TextField placeholder='Enter user name..' />
+    <ContextualSaveBar show={true}/>
+      <Button variant="contained">Button By Harsh</Button>
+      <TextField placeholder="Enter user name.." />
       <br />
       <hr />
       <br />
       <Wrapper sectioned>
         <div>
-          <h1>
-            It is a custom wrapper component!
-          </h1>
+          <h1>It is a custom wrapper component!</h1>
         </div>
-      <Pagination pages={5}/>
-
+        <Pagination pages={5} />
       </Wrapper>
 
       <br />
       <hr />
       <br />
-      
     </>
   );
 }
