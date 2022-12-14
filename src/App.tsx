b90c12fd0e95ea438d6b0aac4595784abf4555ad
Harsh0299-cedcoss <harshsharma2022jun@cedcoss.com>
2022-12-14 13:36:02 +0530

    added pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Button from "./components/Button/Button";
+import Pagination from "./components/Pagination/Pagination";
 
 function App() {
   return (
     <div className="App">
       <Button variant="contained">Button By Harsh</Button>
+      <Pagination pages={5}/>
     </div>
   );
 }
