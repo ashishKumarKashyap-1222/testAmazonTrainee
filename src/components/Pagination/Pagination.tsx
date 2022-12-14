54f4ca4a977a8d4918c3d259a5b04581e38e61a6
KrishR-Cedcoss <krishnamrastogi@cedcoss.com>
2022-12-14 17:10:37 +0530

    added svg icons on button

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -45,7 +45,9 @@ const Pagination = (props: PaginationProps) => {
   return (
     <div className={PaginationCSS.pagination}>
       <button disabled={currentPageInput <= 1} onClick={handlePrev}>
-        <i className="fa-solid fa-chevron-left" />
+        <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" clipRule="evenodd" d="M6.70725 10.2933C7.09825 10.6842 7.09825 11.3162 6.70725 11.7072C6.31625 12.0982 5.68425 12.0982 5.29325 11.7072L0.29325 6.70725C-0.09775 6.31625 -0.09775 5.68425 0.29325 5.29325L5.29325 0.29325C5.68425 -0.09775 6.31625 -0.09775 6.70725 0.29325C7.09825 0.68425 7.09825 1.31625 6.70725 1.70725L2.41425 6.00025L6.70725 10.2933Z" fill="#475569" />
+        </svg>
       </button>
       <form onSubmit={handleSubmit}>
         <input
@@ -59,7 +61,9 @@ const Pagination = (props: PaginationProps) => {
       <span>/</span>
       <span>{props.pages}</span>
       <button disabled={currentPageInput === props.pages} onClick={handleNext}>
-        <i className="fa-solid fa-chevron-right" />
+        <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" clipRule="evenodd" d="M0.293253 10.2933C-0.0977464 10.6842 -0.0977464 11.3162 0.293253 11.7072C0.684253 12.0982 1.31625 12.0982 1.70725 11.7072L6.70725 6.70725C7.09825 6.31625 7.09825 5.68425 6.70725 5.29325L1.70725 0.29325C1.31625 -0.09775 0.684253 -0.09775 0.293253 0.29325C-0.0977464 0.68425 -0.0977464 1.31625 0.293253 1.70725L4.58625 6.00025L0.293253 10.2933Z" fill="#475569" />
+        </svg>
       </button>
     </div>
   );
